99f59bc40b6521851cf1cde0a1534c6217279dda
Azizah Fakhriyyah Zahra <azizahzahra753@gmail.com>
2026-04-20 21:44:35 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,144 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5n3SeK9TJx0":
+        Script1();
+        break;
+      case "637mU1eQJae":
+        Script2();
+        break;
+      case "6nDV52H8WpY":
+        Script3();
+        break;
+      case "60RcrPrN4gh":
+        Script4();
+        break;
+      case "5aFXtdTAdSA":
+        Script5();
+        break;
+      case "5ms6tut11Af":
+        Script6();
+        break;
+      case "6hmDhC9J2JB":
+        Script7();
+        break;
+      case "64YtICY94hI":
+        Script8();
+        break;
+      case "5m9XPalNHZl":
+        Script9();
+        break;
+      case "5d6xcRgMDkX":
+        Script10();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
